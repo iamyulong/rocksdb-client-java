0d2e724275e94a12e15634cd9aa6e75a9b1345ce
Yulong Wu <yulongwu@outlook.com>
2016-09-21 11:51:08 -0400

    Add batch get/put/remove support

--- src/main/java/com/ranksays/rocksdb/client/RocksDB.java
+++ src/main/java/com/ranksays/rocksdb/client/RocksDB.java
@@ -8,6 +8,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Base64;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class RocksDB {
@@ -89,24 +90,53 @@ public class RocksDB {
 	 * @throws IOException
 	 */
 	public byte[] get(String db, byte[] key) throws IOException {
-		if (db == null || key == null) {
+		return getBatch(db, new byte[][] { key })[0];
+	}
+
+	/**
+	 * Get values by keys in a specified database
+	 * 
+	 * @param db
+	 *            database name
+	 * @param keys
+	 *            keys (can not be null, length >= 1)
+	 * @return the retrieved value or null if not exists
+	 * @throws IOException
+	 */
+	public byte[][] getBatch(String db, byte[][] keys) throws IOException {
+		if (db == null || keys == null || keys.length < 1) {
 			throw new IllegalArgumentException();
+		}
+		for (byte[] key : keys) {
+			if (key == null) {
+				throw new IllegalArgumentException("Key can not be null");
+			}
 		}
 		HttpURLConnection con = openConnection("/get");
 
 		JSONObject req = new JSONObject();
 		req.put("db", db);
-		req.put("key", Base64.getEncoder().encodeToString(key));
+		JSONArray keyArr = new JSONArray();
+		for (byte[] key : keys) {
+			keyArr.put(Base64.getEncoder().encodeToString(key));
+		}
+		req.put("keys", keyArr);
 		con.getOutputStream().write(req.toString().getBytes(ENCODING));
 
 		Response resp = Response.fromJSON(parseResponse(con.getInputStream()));
 
 		if (resp.getCode() == Response.CODE_OK) {
-			if (resp.getResults() == null) {
-				return null;
-			} else {
-				return Base64.getDecoder().decode((String) resp.getResults());
+			JSONArray arr = (JSONArray) resp.getResults();
+
+			byte[][] values = new byte[arr.length()][];
+			for (int i = 0; i < arr.length(); i++) {
+				if (arr.isNull(i)) {
+					values[i] = null;
+				} else {
+					values[i] = Base64.getDecoder().decode(arr.getString(i));
+				}
 			}
+			return values;
 		} else {
 			throw new IOException("code: " + resp.getCode() + ", message: " + resp.getMessage());
 		}
@@ -124,15 +154,51 @@ public class RocksDB {
 	 * @throws IOException
 	 */
 	public void put(String db, byte[] key, byte[] value) throws IOException {
-		if (db == null || key == null || value == null) {
+		putBatch(db, new byte[][] { key }, new byte[][] { value });
+	}
+
+	/**
+	 * Set the values of specified keys.
+	 * 
+	 * @param db
+	 *            database name
+	 * @param keys
+	 *            keys (can not be null, length >=1)
+	 * @param values
+	 *            values (can not be null, length >=1)
+	 * @throws IOException
+	 */
+	public void putBatch(String db, byte[][] keys, byte[][] values) throws IOException {
+		if (db == null || keys == null || values == null || keys.length < 1 || values.length < 1) {
 			throw new IllegalArgumentException();
+		}
+		for (byte[] key : keys) {
+			if (key == null) {
+				throw new IllegalArgumentException("Key can not be null");
+			}
+		}
+		for (byte[] value : values) {
+			if (value == null) {
+				throw new IllegalArgumentException("Value can not be null");
+			}
+		}
+		if (keys.length != values.length) {
+			throw new IllegalArgumentException("Number of keys and values does not match");
 		}
 		HttpURLConnection con = openConnection("/put");
 
 		JSONObject req = new JSONObject();
 		req.put("db", db);
-		req.put("key", Base64.getEncoder().encodeToString(key));
-		req.put("value", Base64.getEncoder().encodeToString(value));
+		JSONArray keyArr = new JSONArray();
+		for (byte[] key : keys) {
+			keyArr.put(Base64.getEncoder().encodeToString(key));
+		}
+		req.put("keys", keyArr);
+		JSONArray valueArr = new JSONArray();
+		for (byte[] value : values) {
+			valueArr.put(Base64.getEncoder().encodeToString(value));
+		}
+		req.put("values", valueArr);
 		con.getOutputStream().write(req.toString().getBytes(ENCODING));
 
 		Response resp = Response.fromJSON(parseResponse(con.getInputStream()));
@@ -152,14 +218,36 @@ public class RocksDB {
 	 * @throws IOException
 	 */
 	public void remove(String db, byte[] key) throws IOException {
-		if (db == null || key == null) {
+		removeBatch(db, new byte[][] { key });
+	}
+
+	/**
+	 * Remove values associated with the specified keys.
+	 * 
+	 * @param db
+	 *            database name
+	 * @param keys
+	 *            keys (can not be null, length >= 1)
+	 * @throws IOException
+	 */
+	public void removeBatch(String db, byte[][] keys) throws IOException {
+		if (db == null || keys == null || keys.length < 1) {
 			throw new IllegalArgumentException();
+		}
+		for (byte[] key : keys) {
+			if (key == null) {
+				throw new IllegalArgumentException("Key can not be null");
+			}
 		}
 		HttpURLConnection con = openConnection("/remove");
 
 		JSONObject req = new JSONObject();
 		req.put("db", db);
-		req.put("key", Base64.getEncoder().encodeToString(key));
+		JSONArray keyArr = new JSONArray();
+		for (byte[] key : keys) {
+			keyArr.put(Base64.getEncoder().encodeToString(key));
+		}
+		req.put("keys", keyArr);
 		con.getOutputStream().write(req.toString().getBytes(ENCODING));
 
 		Response resp = Response.fromJSON(parseResponse(con.getInputStream()));
--- src/test/java/com/ranksays/rocksdb/client/RocksDBTest.java
+++ src/test/java/com/ranksays/rocksdb/client/RocksDBTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 
 public class RocksDBTest {
 	@Test
-	public void testAll() throws IOException {
+	public void testBasic() throws IOException {
 		String db = "test";
 		String username = "username";
 		String password = "password";
@@ -19,13 +19,50 @@ public class RocksDBTest {
 		byte[] key = "k".getBytes();
 		byte[] value = "v".getBytes();
 
+		// get
 		assertNull(rdb.get(db, key));
 
+		// put
 		rdb.put(db, key, value);
 		assertArrayEquals(value, rdb.get(db, key));
 
+		// remove
 		rdb.remove(db, key);
 		assertNull(rdb.get(db, key));
+
+		rdb.dropDatabase(db);
+	}
+
+	@Test
+	public void testBatch() throws IOException {
+		String db = "test";
+		String username = "username";
+		String password = "password";
+
+		RocksDB rdb = new RocksDB(username, password);
+
+		byte[][] keys1 = { "k1".getBytes(), "k2".getBytes(), "k3".getBytes() };
+		byte[][] keys2 = { keys1[0], keys1[2] };
+		byte[][] values1 = { "k1".getBytes(), "k2".getBytes(), "k3".getBytes() };
+		byte[][] values2 = { null, values1[1], null };
+
+		// batch put
+		rdb.putBatch(db, keys1, values1);
+
+		// batch get
+		byte[][] values = rdb.getBatch(db, keys1);
+		for (int i = 0; i < values1.length; i++) {
+			assertArrayEquals(values1[i], values[i]);
+		}
+
+		// batch remove
+		rdb.removeBatch(db, keys2);
+
+		// batch get
+		values = rdb.getBatch(db, keys1);
+		for (int i = 0; i < values2.length; i++) {
+			assertArrayEquals(values2[i], values[i]);
+		}
 
 		rdb.dropDatabase(db);
 	}
